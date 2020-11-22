8033fb3a4ca41f0b45796af32f50f0bcc039f174
Xin Wang <xin.wang19@imperial.ac.uk>
2020-11-22 23:00:17 +0200

    Added standard formats and files

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -1,1 +1,18 @@
+module mips_cpu_bus( 
+    /* Standard signals */ 
+    input  logic        clk, 
+    input  logic        reset, 
+    output logic        active, 
+    output logic[31:0]  register_v0, 
+ 
+    /* Avalon memory mapped bus controller (master) */ 
+    input  logic        waitrequest, 
+    input  logic[31:0]  readdata 
+    output logic        write, 
+    output logic        read, 
+    output logic[3:0]   byteenable, 
+    output logic[31:0]  writedata, 
+    output logic[31:0]  address, 
+);
+
 
