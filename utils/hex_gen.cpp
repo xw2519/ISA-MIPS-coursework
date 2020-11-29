5dee3ce8cc459ec327202687c4499239a76a7976
ebby-s <ebbysamson@gmail.com>
2020-11-29 10:46:21 +0000

    Updated rudimentary testing program

--- utils/hex_gen.cpp
+++ utils/hex_gen.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include <iomanip>
+#include<iomanip>
 
 using namespace std;
 
@@ -8,17 +8,29 @@ int main(){
   cout << setw(2);
   cout << setfill('0');
 
-  for(int i=0; i<8192; i++){
-    if(i==5120){
-      cout << "48" << endl;
-    }else if(i==5121){
-      cout << "12" << endl;
+  for(int i=0; i<8192; i++){    // Currently this just generates one program
+    if(i==8){
+      cout << "21" << endl;
+    }else if(i==9){
+      cout << "10" << endl;
+    } else if(i==10){
+      cout << "1F" << endl;
+    } else if(i==12){
+      cout << "08" << endl;
+    }else if(i==5120){
+      cout << "08" << endl;
     } else if(i==5122){
-      cout << "02" << endl;
+      cout << "08" << endl;
     } else if(i==5123){
       cout << "24" << endl;
     } else if(i==5124){
-      cout << "08" << endl;
+      cout << "21" << endl;
+    }else if(i==5125){
+      cout << "81" << endl;
+    }else if(i==5128){
+      cout << "09" << endl;
+    }else if(i==5131){
+      cout << "01" << endl;
     }else{
       cout << "00" << endl;
     }
