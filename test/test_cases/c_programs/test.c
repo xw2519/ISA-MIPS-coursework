6056cb57729ed8774f0dbb9610832ef9faa11cfd
Xin Wang <xin.wang19@imperial.ac.uk>
2020-12-02 23:03:09 +0200

    Cleaned folders and script. added logs.

--- test/test_cases/c_programs/test.c
+++
@@ -1,7 +0,0 @@
-
-
-int main(){
-  int A=2,B=5;
-  int C;
-  C = A * B;
-}
--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Capture the first two command line parameters to specify
-# "SOURCE_DIR" argument that takes source directory, "../rtl" in our case
+# "SOURCE_DIR" argument that takes source directory, "rtl" in our case
 # "INSTRUCTION" argument that takes an instruction to test, test all by default
 SOURCE_DIR="$1"   
 INSTRUCTION="${2:-all}"  
@@ -10,8 +10,8 @@ INSTRUCTION="${2:-all}"
 #echo ${INSTRUCTION} | ../bin/hex_generator > ../test/2-binary/test.hex.txt
 
 iverilog -Wall -g 2012 \
-    -s mips_cpu_bus_tb ../test/0-testbenches/RAM_8x8192_bus.v ../test/0-testbenches/mips_cpu_bus_tb.v ${SOURCE_DIR}/mips_cpu_*.v \
-    -P mips_cpu_bus_tb.RAM_INIT_FILE=\"../test/2-binary/test1.hex.txt\" \
-    -o ../test/3-simulator/mips_bus_test
+    -s mips_cpu_bus_tb test/0-testbenches/RAM_8x8192_bus.v test/0-testbenches/mips_cpu_bus_tb.v ${SOURCE_DIR}/mips_cpu_*.v \
+    -P mips_cpu_bus_tb.RAM_INIT_FILE=\"test/2-binary/addu1.hex.txt\" \
+    -o test/3-simulator/mips_bus_test
     
-../test/3-simulator/mips_bus_test
+test/3-simulator/mips_bus_test
