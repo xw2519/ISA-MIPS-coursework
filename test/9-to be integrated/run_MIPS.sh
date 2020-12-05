1e85cfee7c56436135fd33942920f3e87bd64338
Xin Wang <xin.wang19@imperial.ac.uk>
2020-12-05 23:22:20 +0200

    Modified to the test bench and cleaned up

--- test/9-to be integrated/run_MIPS.sh
+++ test/9-to be integrated/run_MIPS.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
-#effectivly turning on warnings, may want to be changed if we want
-# to continue testing if one test case has faild
+# Turn on warnings
 set -eou pipefail 
 
+# Parameter declarations
 SOURCE_DIRECTORY="$1"
 INSTRUCTION="${2:-all}"  
+TESTCASES="test/1-assembly/*.asm.txt"
 
 #finding last charecter
 LC="${INSTRUCTION: -1}"
 
 
-
-#used to specify all files that are test cases
-TESTCASES="test/1-assembly/*.asm.txt"
-
-#Speciific instructions 
-
-if [[ ${INSTRUCTION} != "all" ]]; then
+if [${INSTRUCTION} != "all"]; then # Instructions specified
     
     if [[ ${LC} =~ [0-9] ]]; then
-        >&2 echo "Running testcase ${INSTRUCTION} only"
-        #Running specific test case
-        ./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${INSTRUCTION}
-    else 
-        #run all testcases with that name (all testcases for one instruction)
-        #STILL DOESNT WORK !!!!!!!
-        >&2 echo "Running all testcases for ${INSTRUCTION}"
-    TESTCS="test/1-assembly/*${INSTRUCTION}.asm.txt" 
-       for i in ${TESTCS}; do 
-            echo "${i}"
-            #TESTNAME_S=$(basename ${i} .txt)
-            #./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME_S}
-        done
+
+		>&2 echo "Running testcase ${INSTRUCTION} only"
+
+		#Running specific test case
+		./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${INSTRUCTION}
+
+	else 
+
+		#run all testcases with that name (all testcases for one instruction)
+
+		>&2 echo "Running all testcases for ${INSTRUCTION}"
+
+		TESTCS="test/1-assembly/*${INSTRUCTION}.asm.txt" 
+		for i in ${TESTCS}; do 
+
+			echo "${i}"
+			
+			#TESTNAME_S=$(basename ${i} .txt)
+			#./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME_S}
+		done
+
     fi
 
-else
-#general case 
->&2 echo "Running all testcases"
-    for i in ${TESTCASES}; do 
-        TESTNAME=$(basename ${i} .asm.txt)
-        ./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME}
-    done
+else # Run all
+
+	>&2 echo "Running all testcases"
+
+	for i in ${TESTCASES}; do 
+	TESTNAME=$(basename ${i} .asm.txt)
+	./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME}
+	done
 fi
 
 
--- test/run_one_testcase.sh
+++ test/run_one_testcase.sh
@@ -1,79 +1,53 @@
 #!/bin/bash
-set -eou pipefail
 
-SOURCE_DIRECTORY="$1"
-TESTCASE="$2"
+set -eou pipefail # Turn on warnings
+rm test/4-output/* 2> /dev/null # Remove all previous output and discard warnings
+
+# Parameter declarations
+SOURCE_DIR="$1"
+TEST_CASE="${2:-all}"
 
 # Redirect output to stder (&2) so that it seperate from genuine outputs
->&2 echo "Testing CPU using ${TESTCASE}"
+>&2 echo "Testing instructions: ${TEST_CASE}"
 
->&2 echo "Assembling input file"
-#To be sorted 
-#either created on the fly or access pre built files 
-
->&2 echo "Compiling test-bench"
-
-#this will need changing so addressing is correct 
-#and memory is initilised correctly 
+>&2 echo "1 - Compiling test-bench"
+iverilog -Wall -g 2012 \
+    -s mips_cpu_bus_tb test/0-testbenches/RAM_8x8192_bus.v test/0-testbenches/mips_cpu_bus_tb.v ${SOURCE_DIR}/mips_cpu_*.v \
+    -P mips_cpu_bus_tb.RAM_INIT_FILE=\"test/2-binary/${TEST_CASE}.hex.txt\" \
+    -o test/3-simulator/mips_cpu_bus_tb_${TEST_CASE}
 
 
-COMP_FILES="${SOURCE_DIRECTORY}""/*.v"
-
-
-iverilog -g 2012 \
-    ${COMP_FILES} test/0-testbenches/mips_cpu_bus_tb.v test/0-testbenches/RAM_*.v \
-    -s mips_cpu_bus_tb \
-   -Pmips_cpu_bus_tb.RAM_INIT_FILE=\"test/2-binary/${TESTCASE}.hex.txt\" \
-   -o test/3-simulator/mips_cpu_bus_tb_${TESTCASE}
-
-
-
->&2 echo "Running test-bench"
-#+e disables automatic script failure if command fails
+>&2 echo "2 - Running test-bench"
+# "set +e" disable automatic script failure if command fails
 set +e 
-test/3-simulator/mips_cpu_bus_tb_${TESTCASE} > test/4-output/mips_cpu_bus_tb_${TESTCASE}.stdout
-#exit code:
-RESULT=$?
-set -e
+test/3-simulator/mips_cpu_bus_tb_${TEST_CASE} > test/4-output/mips_cpu_bus_tb_${TEST_CASE}.stdout
 
-if [[ "${RESULT}" -ne 0 ]]; then 
-    echo "${TESTCASE} FAILED"
-    exit
-fi 
 
-#next steps are to obtain outputs from CPU to then compare them with simmulator or known values
+>&2 echo "3 - Extracting ouputs from CPU"
+Reg_output="TB : INFO : register_v0="
+Active_flag="TB : finished; active=" # To be added in later
+Nothing=""
 
->&2 echo "Extracting ouputs from CPU"
-
-P="TB : INFO : register_v0="
-N=" "
-
-#grep looks at lines containing P
+# Look at lines containing only phrases in Reg_output and Active_flag 
 set +e 
-grep "${P}" test/4-output/mips_cpu_bus_tb_${TESTCASE}.stdout > test/4-output/mips_cpu_bus_tb_${TESTCASE}.out-lines
+grep "${Reg_output}\|${Active_flag}" test/4-output/mips_cpu_bus_tb_${TEST_CASE}.stdout > test/4-output/mips_cpu_bus_tb_${TEST_CASE}.out-lines
 set -e 
 
-#sed replaces unwanted bits (P) with N
-sed -e "s/${P}/${N}/g" test/4-output/mips_cpu_bus_tb_${TESTCASE}.out-lines > test/4-output/mips_cpu_bus_tb_${TESTCASE}.out
+# Replace "TB : INFO : register_v0=" and "TB : finished; active=" with ""
+sed -e "s/${Reg_output}/${Nothing}/g; s/${Active_flag}/${Nothing}/g" test/4-output/mips_cpu_bus_tb_${TEST_CASE}.out-lines > test/4-output/mips_cpu_bus_tb_${TEST_CASE}.out
 
-#obtaining simmulator output....
+# Room for edge cases
 
 
-
-#comparing outputs
->&2 echo "Comparing outputs to simmulators"
-#-w to ignore whitespace //// may be room for edge cases here <----
-
+>&2 echo "4 - Comparing output with reference"
 set +e 
-diff -w test/5-reference/${TESTCASE}.out test/4-output/mips_cpu_bus_tb_${TESTCASE}.out > test/4-output/comparing_errors/${TESTCASE}_comparison_erros.out
+diff -w test/5-reference/${TEST_CASE}.out test/4-output/mips_cpu_bus_tb_${TEST_CASE}.out > test/4-output/${TEST_CASE}_comparison_errors.out
 RESULT=$?
 set -e
 
-#PASS or FAIL
-
 if [[ "${RESULT}" -ne 0 ]]; then 
-    echo " ${TESTCASE}, FAIL"
+    echo "${TEST_CASE}, FAIL"
 else
-    echo " ${TESTCASE}, PASS"
+    echo "${TEST_CASE}, PASS"
 fi
 
