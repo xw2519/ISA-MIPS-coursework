44e4d204ce2eb0fd3c8ef700b0c65da9a67246dc
Xin Wang <xin.wang19@imperial.ac.uk>
2020-12-06 22:43:08 +0200

    Refernce files updated and minor additions to scripts.

--- test/run_one_testcase.sh
+++ test/run_one_testcase.sh
@@ -21,22 +21,22 @@ Case_Instr=$(awk 'NR==8' ${ASSEM_DIR}/${TEST_CASE}.asm.txt)
 Case_Comment=$(awk 'NR==9' ${ASSEM_DIR}/${TEST_CASE}.asm.txt)
 
 # Redirect output to stder (&2) so that it seperate from genuine outputs
->&2 echo "Testing instructions: ${TEST_CASE}"
+# >&2 echo "Testing instructions: ${TEST_CASE}"
 
->&2 echo "1 - Compiling test-bench"
+# >&2 echo "1 - Compiling test-bench"
 iverilog -Wall -g 2012 \
     -s mips_cpu_bus_tb ${TESTBENCH_DIR}/RAM_8x8192_bus.v ${TESTBENCH_DIR}/mips_cpu_bus_tb.v ${SOURCE_DIR}/mips_cpu_*.v \
     -P mips_cpu_bus_tb.RAM_INIT_FILE=\"${BIN_DIR}/${TEST_CASE}.hex.txt\" \
     -o ${SIMUL_DIR}/mips_cpu_bus_tb_${TEST_CASE}
 
 
->&2 echo "2 - Running test-bench"
+# >&2 echo "2 - Running test-bench"
 # "set +e" disable automatic script failure if command fails
 set +e 
 ${SIMUL_DIR}/mips_cpu_bus_tb_${TEST_CASE} > ${OUT_DIR}/${TEST_CASE}.stdout
 
 
->&2 echo "3 - Extracting ouputs from CPU"
+# >&2 echo "3 - Extracting ouputs from CPU"
 Reg_output="TB : INFO : register_v0="
 Active_flag="TB : finished; active=" # To be added in later
 Nothing=""
@@ -49,9 +49,9 @@ set -e
 # Replace "TB : INFO : register_v0=" and "TB : finished; active=" with ""
 sed -e "s/${Reg_output}/${Nothing}/g; s/${Active_flag}/${Nothing}/g" ${OUT_DIR}/${TEST_CASE}.out-lines > ${OUT_DIR}/${TEST_CASE}.out
 
->&2 echo "4 - Comparing output with reference"
+# >&2 echo "4 - Comparing output with reference"
 set +e 
-diff -w ${REF_DIR}/${TEST_CASE}.out ${OUT_DIR}/${TEST_CASE}.out > ${OUT_DIR}/${TEST_CASE}_diff_out
+diff -w ${REF_DIR}/${TEST_CASE}.txt ${OUT_DIR}/${TEST_CASE}.out > ${OUT_DIR}/${TEST_CASE}_diff_out
 RESULT=$?
 set -e
 
--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -2,50 +2,48 @@
 
 set -eou pipefail # Turn on warnings
 
-# Capture the first two command line parameters to specify
-# "SOURCE_DIRECTORY" argument that takes source directory, "rtl" in our case
-# "INSTRUCTION" argument that takes an instruction to test (e.g addiu), test all by default
-# Added third option to test specific test case of instruction (e.g addiu1)
+# Capture the first two command line parameters
+# - "SOURCE_DIR" argument defines source directory: "rtl" in our case
+# - "INSTRUCTION" argument defines instruction to test (e.g addiu): test all by default
+# - Added third option to test specific test case of instruction (e.g addiu1)
 
 
-SOURCE_DIRECTORY="$1"
+SOURCE_DIR="$1"
 INSTRUCTION="${2:-all}"  
 
-# Finding last character, to identify whether INSTRUCTION is "all", a specific test case or all testcases for a given instructiuon
+# Last character identifying whether "INSTRUCTION":
+# - All test cases
+# - Specific test case 
 LAST_CHARACTER="${INSTRUCTION: -1}"
 
-# Used to specify all files that are test cases 
-# For INSTRUCTION == "all"
+# Specify all files that are test cases 
+# "INSTRUCTION" == "all"
 TESTCASES_ALL="test/1-assembly/*.asm.txt"
-
-# For INSTRUCTION == <instruction name>
+# "INSTRUCTION" == <instruction name>
 TESTCASES_INSTR="test/1-assembly/""${INSTRUCTION}""*.asm.txt"
 
-# Identifying which option INSTRUCTION is
+# Identify which option "INSTRUCTION" is
 if [[ ${INSTRUCTION} != "all" ]]; then
     
     if [[ ${LAST_CHARACTER} =~ [0-9] ]]; then
-        >&2 echo "Running testcase ${INSTRUCTION} only"
-# Running specific test case
-        ./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${INSTRUCTION}
+        # >&2 echo "Running testcase ${INSTRUCTION} only"
+        # Run specific test case
+        ./test/run_one_testcase.sh ${SOURCE_DIR} ${INSTRUCTION}
     else 
-# Running all testcases with that name (all testcases for one instruction)
-        >&2 echo "Running all testcases for ${INSTRUCTION}"
-       for i in ${TESTCASES_INSTR}; do 
+        # Run all testcases of instruction (all testcases for one instruction)
+        # >&2 echo "Running all testcases for ${INSTRUCTION}"
+        for i in ${TESTCASES_INSTR}; do 
             TESTNAME_INSTR=$(basename ${i} .asm.txt)
-            ./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME_INSTR}
+            ./test/run_one_testcase.sh ${SOURCE_DIR} ${TESTNAME_INSTR}
         done
     fi
 
 else
+
 # Running general case, testing all by defult 
->&2 echo "Running all testcases"
+# >&2 echo "Running all testcases"
     for i in ${TESTCASES_ALL}; do 
         TESTNAME_ALL=$(basename ${i} .asm.txt)
-        ./test/run_one_testcase.sh ${SOURCE_DIRECTORY} ${TESTNAME_ALL}
+        ./test/run_one_testcase.sh ${SOURCE_DIR} ${TESTNAME_ALL}
     done
 fi
-
-
-
-
