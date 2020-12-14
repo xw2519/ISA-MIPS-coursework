4764b105ce11b9e34c3d2538694b20c2463a350a
Xin Wang <xin.wang19@imperial.ac.uk>
2020-12-14 21:13:13 +0200

    Organised test folders

--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -2,48 +2,58 @@
 
 set -eou pipefail # Turn on warnings
 
-# Capture the first two command line parameters
-# - "SOURCE_DIR" argument defines source directory: "rtl" in our case
-# - "INSTRUCTION" argument defines instruction to test (e.g addiu): test all by default
-# - Added third option to test specific test case of instruction (e.g addiu1)
+# Directory parameters
+ASSEM_DIR="test/1-assembly"
 
-
+# Standard specification input requirement:
+# - "SOURCE_DIR"  - Source directory e.g. "rtl"
+# - "INSTRUCTION" - Instruction to be tested e.g "addiu". Default argument set to "-all"
 SOURCE_DIR="$1"
 INSTRUCTION="${2:-all}"  
 
-# Last character identifying whether "INSTRUCTION":
-# - All test cases
-# - Specific test case 
+# - Custom feature to test the individual test cases of an instruction e.g "addiu1"
+#   -Last character identifying whether "INSTRUCTION":
+#       - All test cases
+#       - Specific test case 
 LAST_CHARACTER="${INSTRUCTION: -1}"
+
+
 
 # Specify all files that are test cases 
 # "INSTRUCTION" == "all"
-TESTCASES_ALL="test/1-assembly/*.asm.txt"
-# "INSTRUCTION" == <instruction name>
-TESTCASES_INSTR="test/1-assembly/""${INSTRUCTION}""*.asm.txt"
 
-# Identify which option "INSTRUCTION" is
-if [[ ${INSTRUCTION} != "all" ]]; then
+# "INSTRUCTION" == <instruction name>
+#TESTCASES_INSTR="test/1-assembly/""${INSTRUCTION}""*.asm.txt"
+
+
+
+if [[ ${INSTRUCTION} != "all" ]]; then # Identify which option "INSTRUCTION" is
     
-    if [[ ${LAST_CHARACTER} =~ [0-9] ]]; then
+    #if [[ ${LAST_CHARACTER} =~ [0-9] ]]; then
         # >&2 echo "Running testcase ${INSTRUCTION} only"
         # Run specific test case
-        ./test/run_one_testcase.sh ${SOURCE_DIR} ${INSTRUCTION}
-    else 
+    #    ./test/run_one_testcase.sh ${SOURCE_DIR} ${INSTRUCTION}
+    #else 
         # Run all testcases of instruction (all testcases for one instruction)
         # >&2 echo "Running all testcases for ${INSTRUCTION}"
-        for i in ${TESTCASES_INSTR}; do 
-            TESTNAME_INSTR=$(basename ${i} .asm.txt)
-            ./test/run_one_testcase.sh ${SOURCE_DIR} ${TESTNAME_INSTR}
+    #    for i in ${TESTCASES_INSTR}; do 
+    #        TESTNAME_INSTR=$(basename ${i} .asm.txt)
+    #        ./test/run_one_testcase.sh ${SOURCE_DIR} ${TESTNAME_INSTR}
+    #    done
+    #fi
+    echo "HI"
+
+else # Default case: Run all testcases
+
+    for TEST_FOLDER in ${ASSEM_DIR}/*; 
+    do
+        TEST_TYPE="$(basename -- $TEST_FOLDER)"
+
+        for i in ${TEST_FOLDER}/*.asm.txt; do 
+            TEST_CASE="$(basename -- $i)"
+            TEST_CASE=${TEST_CASE//".asm.txt"/} # Remove ".asm.txt"
+            ./test/run_one_testcase.sh ${SOURCE_DIR} ${TEST_CASE} ${TEST_TYPE}
         done
-    fi
-
-else
-
-# Running general case, testing all by defult 
-# >&2 echo "Running all testcases"
-    for i in ${TESTCASES_ALL}; do 
-        TESTNAME_ALL=$(basename ${i} .asm.txt)
-        ./test/run_one_testcase.sh ${SOURCE_DIR} ${TESTNAME_ALL}
+        
     done
 fi
