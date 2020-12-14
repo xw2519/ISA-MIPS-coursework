61f587cbe13f0ac5d08851dce3e8cc53cbd1bd72
Xin Wang <xin.wang19@imperial.ac.uk>
2020-12-14 17:33:57 +0200

    Cleaned up directory

--- test/test_code/test2.s
+++
@@ -1,9 +0,0 @@
-.data
-var1:      .word      5
-
-.text
-main:
-lw      $t0, var1
-addiu   $t0, $t0, -40
-
-
--- test/test_code/test5.s
+++
@@ -1,9 +0,0 @@
-.data
-var1:      .word      43981     #creating integer of hex abcd
-var2:      .word      5
-
-.text
-main:
-lw		$s1, var2		
-addi    $s1, $s2, 40
-addi    $s1, $s2, 40
