c433d0eee79e2346ca7a3ddccb871ba9022ac51d
kt1719 <kt1719@ic.ac.uk>
2020-12-20 15:08:15 +0000

    fibonacci testcase
    Signed-off-by: kt1719 <kt1719@ic.ac.uk>

--- test/assembler.sh
+++ test/assembler.sh
@@ -6,3 +6,4 @@ set -eou pipefail # Turn on warnings
 python3 utils/assembler.py test/1-assembly/0-Fundemental/ test/2-binary/0-Fundemental/ -v
 python3 utils/assembler.py test/1-assembly/1-Dependent/ test/2-binary/1-Dependent/ -v
 python3 utils/assembler.py test/1-assembly/2-Dependent/ test/2-binary/2-Dependent/ -v
+python3 utils/assembler.py test/1-assembly/3-Integration/ test/2-binary/3-Integration/ -v
