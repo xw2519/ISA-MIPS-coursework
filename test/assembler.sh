6c14b7e239a5cc6768b13a68aaef847c47bfed2a
kt1719 <kt1719@ic.ac.uk>
2020-12-20 13:03:34 +0000

    all instructions sorted
    Signed-off-by: kt1719 <kt1719@ic.ac.uk>

--- test/assembler.sh
+++ test/assembler.sh
@@ -3,8 +3,6 @@
 set -eou pipefail # Turn on warnings
 
 
-python3 utils/assembler.py test/1-assembly/0-Independent/ test/2-binary/0-Independent/ -v
+python3 utils/assembler.py test/1-assembly/0-Fundemental/ test/2-binary/0-Fundemental/ -v
 python3 utils/assembler.py test/1-assembly/1-Dependent/ test/2-binary/1-Dependent/ -v
 python3 utils/assembler.py test/1-assembly/2-Dependent/ test/2-binary/2-Dependent/ -v
-python3 utils/assembler.py test/1-assembly/3-Fundamental/ test/2-binary/3-Fundamental/ -v
-python3 utils/assembler.py test/1-assembly/5-I-type/ test/2-binary/5-I-type/ -v
